6a65db403536d355add59da776bdff101a2a2b90
Somya <somya1234@gmail.com>
2020-02-25 18:09:29 +0530

    more into

---
+++ lecture2/activity/commands/untreefy.js
@@ -0,0 +1,5 @@
+module.exports.untreefy = function(){
+    console.log("untreefy function is called");
+    src = arguments[0];
+    dest = arguments[1];
+}
---
+++ lecture2/activity/tpp.js
@@ -0,0 +1,28 @@
+let cmd = process.argv[2];
+let viewFile = require("./commands/view");
+let treefyFile = require("./commands/treefy");
+let untreefyFile = require("./commands/untreefy");
+let helpFile = require("./commands/help");
+let monitorFile = require("./commands/monitor");
+// switch(process.argv[2]){
+    // or
+switch(cmd){
+    case "view":
+       viewFile.view(process.argv[3],process.argv[4]);
+        break;
+    case  "untreefy":
+        untreefyFile.untreefy(process.argv[3],process.argv[4]);
+        break;
+    case "treefy":
+       treefyFile.treefy(process.argv[3],process.argv[4]);
+        break;
+    case "monitor":
+       monitorFile.monitor(process.argv[3],process.argv[4]);
+        break;
+    case "help":
+        helpFile.help();
+        break;
+    default:
+        console.log("Wrong command");
+        break;
+}
