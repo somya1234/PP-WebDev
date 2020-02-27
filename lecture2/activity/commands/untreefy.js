593ba87352b49dc754e2e3d3b90e76b5e2a78d47
Somya <somya1234@gmail.com>
2020-02-27 16:24:43 +0530

    untreefy doing.

--- lecture2/activity/commands/untreefy.js
+++ lecture2/activity/commands/untreefy.js
@@ -2,4 +2,20 @@ module.exports.untreefy = function(){
     console.log("untreefy function is called");
     src = arguments[0];
     dest = arguments[1];
+    untreefy(src,dest);
+}
+
+function untreefy(src,dest){
+    let ans = fs.lstatSync(src).isDirectory();
+    if(ans==false){
+        let uniqueName = uniqid();
+        fs.copyFileSync(src, path.join(dest,uniqueName));
+        console.log("copied a new file");
+    } else {
+        let children = fs.readdirSync(src);
+        for(let i=0; i<children.length; i++){
+            let cPath = path.join(src,children[i]);
+            untreefy(cPath,dest);
+        }
+    }
 }
--- lecture2/facts/testUntreefy.js
+++ lecture2/facts/testUntreefy.js
@@ -2,19 +2,53 @@ let fs = require("fs");
 let path = require("path");
 const uniqid = require('uniqid');
 
-function untreefy(src,dest){
+let root = {};
+
+// untreefyFolder(src,dest,root);
+// console.log(root);
+// console.log("All files have been copied");
+
+function untreefyFolder(src,dest,node){
     let ans = fs.lstatSync(src).isDirectory();
-    if(ans==false){
+    if(ans == false){
         let uniqueName = uniqid();
-        fs.copyFileSync(src, path.join(dest,uniqueName));
-        console.log("copied a new file");
+        node.isFile = true;
+        node.name = path.basename(src);
+        node.newName = uniqueName;
+        fs.copyFileSync(src,path.join(dest,uniqueName));
+        console.log(src);
+        console.log(path.join(dest,uniqueName));
     } else {
-        let children = fs.readdirSync(src);
-        for(let i=0; i<children.length; i++){
-            let cPath = path.join(src,children[i]);
-            untreefy(cPath,dest);
+        node.isFile = false;
+        node.name = path.basename(src);
+        node.children = [];
+        let childrens = fs.readdirSync(src);
+        for(let i=0; i<childrens.length; i++){
+            let childObj = {};
+            let cPath = path.join(src,childrens[i]);
+            untreefyFolder(cPath, dest,childObj);
+            node.children.push(childObj);
+            
         }
     }
 }
+console.log("All files have been copied");
 
-untreefy("/home/somya/Documents/PP-WebDev/lecture2/src/d10", "/home/somya/Documents/PP-WebDev/lecture2/dest");
+// function untreefy(src,dest,root){
+    //     let ans = fs.lstatSync(src).isDirectory();
+    //     if(ans==false){
+        //         let uniqueName = uniqid();
+//         fs.copyFileSync(src, path.join(dest,uniqueName));
+//         console.log("copied a new file");
+//     } else {
+    //         let children = fs.readdirSync(src);
+    //         for(let i=0; i<children.length; i++){
+//             let cPath = path.join(src,children[i]);
+//             untreefy(cPath,dest);
+//         }
+//     }
+// }
+
+// untreefy("/home/somya/Documents/PP-WebDev/lecture2/src/d10", "/home/somya/Documents/PP-WebDev/lecture2/dest");
+untreefyFolder("/home/somya/Documents/PP-WebDev/lecture2/src", "/home/somya/Documents/PP-WebDev/lecture2/dest",root);
+console.log(root);
