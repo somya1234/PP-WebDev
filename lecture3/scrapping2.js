f0fc1b19a3c72064f4c00e3be0da43cec42f81c0
Somya <somya1234@gmail.com>
2020-02-29 11:48:09 +0530

    working for commentary.2

--- lecture3/scrapping2.js
+++ lecture3/scrapping2.js
@@ -3,9 +3,9 @@ let cheerio = require("cheerio");
 let fs = require("fs");
 
 request("https://www.espncricinfo.com/series/19322/commentary/1187679", function(err, res , html){
-    // console.log(res);
+    console.log(res);
     if(err == null && res.statusCode == 200){
-        fs.writeFileSync("abc.html",html);
+        // fs.writeFileSync("abc.html",html);
         parseHtml(html);
         console.log("File written");
     } else {
